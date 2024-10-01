4ae7d2913cdd6f0f37d67065dbae82bc019e939b
Erwan <46385057+LaLoutreCosmique@users.noreply.github.com>
2024-10-01 17:05:06 +0200

    push

--- js/cursor.js
+++ js/cursor.js
@@ -6,13 +6,14 @@ var cursor = {
     endY: (window.innerHeight / 2),
     isVisible: true,
     isHovering: false,
+    isActive: false,
     $dot: document.querySelector('#cursor-dot'),
-    $outline: document.querySelector('#cursor-light'),
+    $light: document.querySelector('#cursor-light'),
 
     init: function () {
         // Set up element sizes
         this.dotSize = this.$dot.offsetWidth;
-        this.outlineSize = this.$outline.offsetWidth;
+        this.lightSize = this.$light.offsetWidth;
 
         this.setupEventListeners();
         this.animateDotLight();
@@ -35,21 +36,15 @@ var cursor = {
 
         // Click events
         document.addEventListener('mousedown', function () {
-            //self.isHovering = true;
-            //self.toggleHoverAnim();
+            self.isActive = true;
+            self.toggleActiveAnim();
         });
         document.addEventListener('mouseup', function () {
-            //self.isHovering = false;
-            //self.toggleHoverAnim();
+            self.isActive = false;
+            self.toggleActiveAnim();
         });
 
-
         document.addEventListener('mousemove', function (e) {
-            // Show the cursor
-            //self.isVisible = true;
-            //self.toggleCursorVisibility();
-
-            // Position the dot
             self.endX = e.pageX;
             self.endY = e.pageY;
             self.$dot.style.top = self.endY + 'px';
@@ -61,15 +56,19 @@ var cursor = {
             self.isVisible = true;
             self.toggleCursorVisibility();
             self.$dot.style.opacity = 1;
-            self.$outline.style.opacity = 1;
+            self.$light.style.opacity = 1;
         });
 
         document.addEventListener('mouseleave', function (e) {
             self.isVisible = false;
             self.toggleCursorVisibility();
             self.$dot.style.opacity = 0;
-            self.$outline.style.opacity = 0;
+            self.$light.style.opacity = 0;
         });
+    },
+
+    moveDot: function() {
+        
     },
 
     animateDotLight: function () {
@@ -77,8 +76,8 @@ var cursor = {
 
         self._x += (self.endX - self._x) / self.delay;
         self._y += (self.endY - self._y) / self.delay;
-        self.$outline.style.top = self._y + 'px';
-        self.$outline.style.left = self._x + 'px';
+        self.$light.style.top = self._y + 'px';
+        self.$light.style.left = self._x + 'px';
 
         requestAnimationFrame(this.animateDotLight.bind(self));
     },
@@ -86,12 +85,24 @@ var cursor = {
     toggleHoverAnim: function () {
         var self = this;
 
+        if (self.isActive) return;
+
         if (self.isHovering) {
-            self.$dot.style.transform = 'translate(-50%, -50%) scale(0.7)';
-            self.$outline.style.transform = 'translate(-50%, -50%) scale(0)';
+            self.$dot.style.transform = 'translate(-50%, -50%) scale(4)';
+            self.$light.style.transform = 'translate(-50%, -50%) scale(0)';
         } else {
             self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
-            self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+            self.$light.style.transform = 'translate(-50%, -50%) scale(1)';
+        }
+    },
+
+    toggleActiveAnim: function () {
+        var self = this;
+
+        if (self.isActive) {
+            self.$dot.style.transform = 'translate(-50%, -50%) scale(0.7)';
+        } else {
+            self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
         }
     },
 
@@ -100,10 +111,10 @@ var cursor = {
 
         if (self.isVisible) {
             self.$dot.style.opacity = 1;
-            self.$outline.style.opacity = 1;
+            self.$light.style.opacity = 1;
         } else {
             self.$dot.style.opacity = 0;
-            self.$outline.style.opacity = 0;
+            self.$light.style.opacity = 0;
         }
     }
 }
