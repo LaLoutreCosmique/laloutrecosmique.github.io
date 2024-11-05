10939aaab6022b7111569dda59c0d4726cc2e706
La Loutre Cosmique <sushi.laque77@gmail.com>
2024-11-05 11:21:15 +0100

    feat: direction anim

--- js/main.js
+++ js/main.js
@@ -94,16 +94,37 @@ var pizzaRavioli = {
   }
 }
 
+
+//
+// PAGE IDs :
+// 0 : home (bigger hero)
+// 1 : projects
+// 2 : discover me
+// 3 : career (jobs & schools)
+//
+// NAVIGATION :
+// Left nav-btn -> current page ID - 1
+// Right nav-btn -> current page ID + 1
+// Loop (0,1,2,3,0,1,2,3,0,1,2,...)
+//
+
 var menuNavigation = {
   navButton: null,
   heroMoves: false,
   $hero: null,
+  isMoving: false,
+  currentPageID: 0,
+  pagesTitle: {
+    0: "Accueil",
+    1: "Mes projets"
+  },
 
   init: function () {
     var self = this;
 
     this.$hero = document.querySelector('.hero');
     this.$subHero = document.querySelector('.sub-hero');
+    this.$fadeElem = document.querySelectorAll('.anim-fade');
 
     document.querySelectorAll('.nav-left a').forEach(function (el) {
       el.addEventListener('click', function () {
@@ -113,27 +134,43 @@ var menuNavigation = {
 
     document.querySelectorAll('.nav-right a').forEach(function (el) {
       el.addEventListener('click', function () {
-        self.navLeft();
+        self.navRight();
       })
     })
 
     document.querySelectorAll('.nav-down a').forEach(function (el) {
       el.addEventListener('click', function () {
-        self.navLeft();
+        self.navDown();
       })
     })
   },
 
   navLeft: function () {
-    this.moveHero();
+    if (this.isMoving === true) return;
+
+    var newPageID = (this.currentPageID - 1) < 0 ? 3 : this.currentPageID - 1;
+    this.doAnimations("left", newPageID);
   },
 
   navRight: function () {
-    this.moveHero();
+    if (this.isMoving === true) return;
+
+    var newPageID = (this.currentPageID - 1) > 3 ? 0 : this.currentPageID + 1;
+    this.doAnimations("right");
   },
 
   navDown: function () {
+    if (this.isMoving === true) return;
+
+    this.doAnimations("down", this.currentPageID);
+  },
+
+  doAnimations: function (dir, newPageID) {
+    this.isMoving = true;
+
     this.moveHero();
+    this.fadeAnim();
+    this.moveSection(dir);
   },
 
   moveHero: function () {
@@ -142,7 +179,126 @@ var menuNavigation = {
       this.$hero.style.top = '3%';
       this.$hero.style.fontSize = '3rem';
 
-      this.$subHero.style.top = '13%';
+      this.$subHero.style.top = '10%';
     }
+  },
+
+  fadeAnim: function () {
+    if (this.$fadeElem) {
+      anime.timeline({ loop: false })
+        .add({
+          targets: '.anim-fade',
+          opacity: [1, 0],
+          easing: "linear",
+          duration: 200
+        });
+    }
+  },
+
+  unfadeAnim: function () {
+    if (this.$fadeElem) {
+      anime.timeline({ loop: false })
+        .add({
+          targets: '.anim-fade',
+          opacity: [0, 1],
+          easing: "linear",
+          duration: 200
+        });
+    }
+  },
+
+  moveSection: function (dir) {
+    var self = this;
+
+    switch (dir) {
+      // --- LEFT NAV BUTTON --- //
+      case "left":
+        anime.timeline({ loop: false })
+        .add({
+          targets: '.section-container',
+          translateX: [0, 100],
+          opacity: [1, 0],
+          easing: "easeInCubic",
+          duration: 700,
+          complete: function (anim) { 
+            self.setSubHero();
+            self.unfadeAnim();
+          }
+      })
+      .add({
+        targets: '.section-container',
+        translateX: [-100, 0],
+        opacity: [0, 1],
+        easing: "easeOutQuart",
+        duration: 1000,
+        delay: 300,
+        complete: function (anim) { 
+          self.isMoving = false;  
+        }
+    });
+        break;
+        
+      // --- RIGHT NAV BUTTON --- //
+      case "right":
+        anime.timeline({ loop: false })
+        .add({
+          targets: '.section-container',
+          translateX: [0, -100],
+          opacity: [1, 0],
+          easing: "easeInCubic",
+          duration: 700,
+          complete: function (anim) { 
+            self.setSubHero();
+            self.unfadeAnim();
+          }
+      })
+      .add({
+        targets: '.section-container',
+        translateX: [100, 0],
+        opacity: [0, 1],
+        easing: "easeOutQuart",
+        duration: 1000,
+        delay: 300,
+        complete: function (anim) { 
+          self.isMoving = false;   
+        }
+    });
+        break;
+    
+      // --- DOWN NAV BUTTON --- //
+      case "down":
+        anime.timeline({ loop: false })
+        .add({
+          targets: '.section-container',
+          translateY: [0, -100],
+          opacity: [1, 0],
+          easing: "easeInCubic",
+          duration: 700,
+          complete: function (anim) { 
+            self.setSubHero();
+            self.unfadeAnim();
+          }
+      })
+      .add({
+        targets: '.section-container',
+        translateY: [100, 0],
+        opacity: [0, 1],
+        easing: "easeOutQuart",
+        duration: 1000,
+        delay: 300,
+        complete: function (anim) { 
+          self.isMoving = false;
+        }
+    });
+        break;
+      
+      default:
+        console.error('HUH ?? Bad direction bro');
+        break;
+    }
+  },
+
+  setSubHero: function () {
+    this.$subHero.innerHTML = this.pagesTitle[0];
   }
 }
